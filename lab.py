c2533e2076ba6efc5bb6b7d94eb4ad80f5aaf816
Łukasz Janus <men1985@wp.pl>
2017-06-05 13:56:44 +0200

    python file

---
+++ lab.py
@@ -0,0 +1,8 @@
+age = int(input("How old are you? "))
+print ("In 10 years you will be", (age + 10))
+print ("In 20 years you will be", (age + 20))
+print ("Half a lifetime ago you were", (age / 2))
+if age >= 18:
+    print ("You are eligible to vote in the US")
+else:
+    print ("You are too young to vote in the US")
---
+++ square.py
@@ -0,0 +1,6 @@
+print ("Square Calculator")
+width = 8
+height = 8
+area = width * height
+print ("The area of the square is", area)
+print ("***EXECUTION COMPLETE***")
